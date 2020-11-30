69e8328d6ed42aec5a92f60363fb204d241b965f
Kiznaiver27 <a19samvigpul@inspedralbes.cat>
2020-11-30 23:38:02 +0100

    Registro

---
+++ Assets/Scripts/Samuel/Networking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Networking : MonoBehaviour
+{
+    void Start()
+    {
+        //Connect with the server
+    }
+
+    public void sendNewUser(string username, string name, string password, string surname)
+    {
+        //Send username, name, password (encrypt), surname
+    }
+    void Update()
+    {
+        
+    }
+}
