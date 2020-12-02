38880a5ef45a26086a28573d34871232e3eee632
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2020-12-02 19:51:21 +0100

    Pantalla de Carga

---
+++ Assets/Scripts/Marc/Loading.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    private float speed = 4f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+}
