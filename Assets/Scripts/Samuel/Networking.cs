128d1f0fc0a0b531b6f753f79a362e6d38c050a5
Kiznaiver27 <60893142+Kiznaiver27@users.noreply.github.com>
2020-12-10 23:47:52 +0100

    Agregando Post y Get con ApiRest
    He agregado un el sistema para pedir al servidor un usuario a travez de un GET y tambien la forma de agregar un usuario haciendo una peticion POST.
    Para que funcione hay que enlazar cada uno con los InputField correspondientes

--- Assets/Scripts/Samuel/Networking.cs
+++ Assets/Scripts/Samuel/Networking.cs
@@ -4,17 +4,69 @@ using UnityEngine;
 
 public class Networking : MonoBehaviour
 {
+    [SerializeField]
+    private InputField inputAlias, inputNom, inputCognom;
+    public bool localOrHost = false;
+    public int position, score;
+    private string url;
     void Start()
     {
-        //Connect with the server
+        if (localOrHost)
+        {
+            url = "http://samvp.herokuapp.com/players/";
+        }
+        else
+        {
+            url = "http://127.0.0.1:4567/players/";
+        }
     }
 
     public void sendNewUser(string username, string name, string password, string surname)
     {
         //Send username, name, password (encrypt), surname
     }
-    void Update()
+    public void SendPlayer()
     {
-        
+        StartCoroutine(Post());
+    }
+    public void GetPlayer()
+    {
+        StartCoroutine(GetText());
+    }
+    IEnumerator Post()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", inputNom.text);
+        form.AddField("surname", inputCognom.text);
+        UnityWebRequest peticion = UnityWebRequest.Post(url + inputAlias.text, form);
+
+        yield return peticion.SendWebRequest();
+
+        if (peticion.isNetworkError)
+        {
+            Debug.Log(peticion.error);
+        }
+        else
+        {
+            Debug.Log(peticion.downloadHandler.text);
+        }
+    }
+    IEnumerator GetText()
+    {
+        UnityWebRequest peticion = UnityWebRequest.Get(url + inputAlias.text);
+        yield return peticion.Send();
+
+        if (peticion.isNetworkError)
+        {
+            Debug.Log(peticion.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(peticion.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = peticion.downloadHandler.data;
+        }
     }
 }
